56135ce05955c31264345a3cd54b750933863960
Henri Ly <henri@softlydev.com>
2023-07-26 19:52:21 +0200

    use json for config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 import { Menu } from "./component/Menu"
-
-const configExample = {
-  name: "designplox",
-  children: [
-    { name: "artists", children: [{ name: "mickael jackson", children: [{ name: "britney spears" }, { name: "usher" }] }, { name: "kayne west" }, { name: "christophe mae" }] },
-    { name: "albums", children: [{ name: "get your wings" }, { name: "hotel california" }, { name: "phisical graffiti", children: [{ name: "houses of the holy" }, { name: "in my time of dying" }] }] },
-    { name: "songs", children: [{ name: "d" }, { name: "e" }] },
-    { name: "genres", children: [{ name: "rock" }, { name: "rnb" }] },
-    { name: "settings", children: [{ name: "name" }, { name: "i", children: [{ name: "2", children: [{ name: "3" }] }] }] },
-  ]
-}
+import config from "./menu.json";
 
 function App() {
   return (
@@ -17,7 +7,7 @@ function App() {
       <div className="mb-5">
         <h1 className="text-center text-4xl">Test Chanel</h1>
       </div>
-      <Menu config={configExample} />
+      <Menu config={config} />
     </div>
   )
 }
